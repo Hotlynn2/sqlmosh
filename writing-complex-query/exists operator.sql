2227cce8d3ba374dc8bf1eeed59cc99bbe73989a
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-11 07:24:56 +0000

    exists operator solution 2

--- writing-complex-query/exists operator.sql
+++ writing-complex-query/exists operator.sql
@@ -1,8 +1,17 @@
 USE sql_store;
 
-SELECT * 
+/* SELECT * 
 FROM products
 WHERE product_id NOT IN (SELECT product_id
 						FROM order_items
                         )
+*/
+
+SELECT * 
+FROM products p
+WHERE NOT EXISTS (SELECT product_id
+					FROM order_items
+                    WHERE product_id = p.product_id
+                    )
+					
  
