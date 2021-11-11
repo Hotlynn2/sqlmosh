ce7cd8875f18f22bb74a90d05d1b5e858e020280
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-11 08:15:44 +0000

    subqueries in select clause

---
+++ writing-complex-query/subqueries in select clause.sql
@@ -0,0 +1,11 @@
+USE sql_invoicing;
+
+SELECT client_id,
+		name ,
+        (SELECT SUM (invoice_total) FROM invoices
+         WHERE client_id = c.client_id) AS total_sales,
+        (SELECT AVG(invoice_total) FROM invoices
+        WHERE client_id = c.client_id) AS average
+FROM clients c
+        
+        
