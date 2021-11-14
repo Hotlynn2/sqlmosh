7ed65cf97114d6d30b68e6a3b56b15ce3c7dfaed
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-14 01:11:25 +0000

    string_functions1

---
+++ essential_mysql_functions/string_functions.sql
@@ -0,0 +1,13 @@
+SELECT LENGTH('Hotspot') AS length,
+		LOWER('Hotspot') AS lower_case,
+        UPPER('Hotspot') AS upper_case,
+        LTRIM('   Hotspot') AS left_trim,
+        RTRIM('Hotspot   ') AS right_trim,
+        TRIM('   Hotspot   ') AS lead_lag_spaces,
+        LEFT('Hotspot', 3) AS first_letters_left,
+        RIGHT('Hotspot', 4) AS last_letters_right,
+        SUBSTRING('Hotspot', 4) AS substring1,
+        SUBSTRING('Hotspot', 3, 6) AS substring2,
+        LOCATE('t', 'Hotspot') AS locate,
+        REPLACE('Hotspot', 'Hot', 'Fire') AS replace_string,
+        CONCAT('Hot', ' spot') AS concat
