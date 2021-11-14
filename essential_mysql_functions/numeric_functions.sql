104cb06f7422469142d733a72450c5a1010aa50c
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-14 00:33:50 +0000

    numeric_functions

---
+++ essential_mysql_functions/numeric_functions.sql
@@ -0,0 +1,7 @@
+SELECT  ROUND(6.3475, 2) AS two_decimal,
+		ROUND(9.478, 1) AS one_decimal,
+        CEILING(5.1) AS ceiling,
+        TRUNCATE(53.958, 2) AS truncate,
+        FLOOR(6.78) AS floor,
+        ABS(-98.7) AS absolute,
+        RAND() AS random_number
