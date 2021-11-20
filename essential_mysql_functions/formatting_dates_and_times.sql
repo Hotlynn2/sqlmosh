6f18bd8443fa9c2c56ff2cbbe459fb43c4107636
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-20 15:16:05 +0000

    data and time last addition

--- essential_mysql_functions/formatting_dates_and_times.sql
+++ essential_mysql_functions/formatting_dates_and_times.sql
@@ -1,4 +1,4 @@
 SELECT TIME(now()) AS updated_time, DATE(NOW()) AS updated_date, 
-DATE_FORMAT(NOW(), '%M-%D-%Y') as format_date, TIME_FORMAT(NOW(), '%H,%i,%s') as format_time
+DATE_FORMAT(NOW(), '%M-%D-%Y') as format_date, TIME_FORMAT(NOW(), '%H,%i,%s, %p') as format_time
 
 -- https://dev.mysql.com/doc/refman/8.0/en/date-and-time-functions.html
