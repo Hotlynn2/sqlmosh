6d0cf5d1e0db0b50c7d1daf8a48ce3acd9c70d95
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-20 15:04:33 +0000

    link to date and time functions

--- essential_mysql_functions/formatting_dates_and_times.sql
+++ essential_mysql_functions/formatting_dates_and_times.sql
@@ -1,2 +1,4 @@
 SELECT TIME(now()) AS updated_time, DATE(NOW()) AS updated_date, 
-DATE_FORMAT(NOW(), '%M-%D-%Y') as format_date, TIME_FORMAT(NOW(), '%H,%i,%s')
+DATE_FORMAT(NOW(), '%M-%D-%Y') as format_date, TIME_FORMAT(NOW(), '%H,%i,%s') as format_time
+
+-- https://dev.mysql.com/doc/refman/8.0/en/date-and-time-functions.html
