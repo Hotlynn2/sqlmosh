14f92e39d2778c0850834e80873885a8b7b46a70
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-23 23:19:01 +0000

    if function solution

---
+++ essential_mysql_functions/if_function.sql
@@ -0,0 +1,13 @@
+USE sql_store;
+
+select 
+	p.product_id,
+    p.name,
+    o.quantity AS orders,
+    if(o.quantity > 1, 'Many times', 'Once') AS frequency
+from
+	products p 
+join order_items o
+on p.product_id = o.product_id
+
+    
