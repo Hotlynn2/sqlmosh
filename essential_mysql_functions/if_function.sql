a63e3b4ef1fe7cdc2a42a0ce3f75eb1dc81283d6
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-23 23:35:28 +0000

    if function solution rectified

--- essential_mysql_functions/if_function.sql
+++ essential_mysql_functions/if_function.sql
@@ -3,11 +3,12 @@ USE sql_store;
 select 
 	p.product_id,
     p.name,
-    o.quantity AS orders,
-    if(o.quantity > 1, 'Many times', 'Once') AS frequency
+    COUNT(*) AS orders,
+    if(COUNT(*) > 1, 'Many times', 'Once') AS frequency
 from
 	products p 
 join order_items o
 on p.product_id = o.product_id
+GROUP BY p.product_id, p.name
 
     
