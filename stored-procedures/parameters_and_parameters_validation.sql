fddcb761d6856a7e5426d7ce6148560922afa471
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-11-26 22:09:13 +0000

    validating parameters

--- stored-procedures/parameters_and_parameters_validation.sql
+++ stored-procedures/parameters_and_parameters_validation.sql
@@ -4,9 +4,14 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_payment_details`(
     payment_date DATE
 )
 BEGIN
+IF payment_amount < 0 THEN
+	SIGNAL SQLSTATE '22003' 
+		SET MESSAGE_TEXT = 'Invalid Input';
+END IF;
 	UPDATE invoices i
 	SET
 		i.payment_total = payment_amount,
         i.payment_date = payment_date
 	WHERE i.invoice_id = invoice_id;
 END
+
