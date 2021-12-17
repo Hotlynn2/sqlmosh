9aa33790c1a7b0636a5521beb33d90f7a82e85eb
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-12-17 12:44:55 +0000

    updated model_script

--- designing-databases/model_script_file.sql
+++ designing-databases/model_script_file.sql
@@ -1,114 +1,77 @@
--- MySQL Workbench Forward Engineering
+-- MySQL Workbench Synchronization
+-- Generated: 2021-12-17 12:44
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: dell
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema school
--- -----------------------------------------------------
+ALTER TABLE `school`.`courses` 
+DROP FOREIGN KEY `fk_courses_instructors1`;
 
--- -----------------------------------------------------
--- Schema school
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `school` DEFAULT CHARACTER SET utf8 ;
-USE `school` ;
+ALTER TABLE `school`.`enrollments` 
+DROP FOREIGN KEY `fk_enrollments_courses1`;
 
--- -----------------------------------------------------
--- Table `school`.`students`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`students` (
-  `student_ID` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(50) NULL,
-  `last_name` VARCHAR(50) NULL,
-  `email` VARCHAR(255) NULL,
-  `date_registered` DATETIME NULL,
-  PRIMARY KEY (`student_ID`))
-ENGINE = InnoDB;
+ALTER TABLE `school`.`courses_tags` 
+DROP FOREIGN KEY `fk_courses_tags_courses1`,
+DROP FOREIGN KEY `fk_courses_tags_tags1`;
 
+ALTER TABLE `school`.`students` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
--- -----------------------------------------------------
--- Table `school`.`instructors`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`instructors` (
-  `instructors_ID` SMALLINT NOT NULL,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`instructors_ID`))
-ENGINE = InnoDB;
+ALTER TABLE `school`.`courses` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `school`.`enrollments` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
+ADD COLUMN `coupon` VARCHAR(50) NULL DEFAULT NULL AFTER `price`,
+CHANGE COLUMN `date` `date` DATETIME NOT NULL ,
+CHANGE COLUMN `price` `price` DECIMAL(5,2) NOT NULL ;
 
--- -----------------------------------------------------
--- Table `school`.`courses`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`courses` (
-  `course_ID` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(255) NULL,
-  `price` DECIMAL(5,2) NULL,
-  `tags` VARCHAR(255) NULL,
-  `instructors_ID` SMALLINT NOT NULL,
-  PRIMARY KEY (`course_ID`),
-  INDEX `fk_courses_instructors1_idx` (`instructors_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_courses_instructors1`
-    FOREIGN KEY (`instructors_ID`)
-    REFERENCES `school`.`instructors` (`instructors_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ALTER TABLE `school`.`tags` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `school`.`courses_tags` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
--- -----------------------------------------------------
--- Table `school`.`enrollments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`enrollments` (
-  `student_ID` INT NOT NULL,
-  `course_ID` INT NOT NULL,
-  `date` DATETIME NULL,
-  `price` DECIMAL(5,2) NULL,
-  INDEX `fk_enrollments_students_idx` (`student_ID` ASC) VISIBLE,
-  INDEX `fk_enrollments_courses1_idx` (`course_ID` ASC) VISIBLE,
-  PRIMARY KEY (`student_ID`, `course_ID`),
-  CONSTRAINT `fk_enrollments_students`
-    FOREIGN KEY (`student_ID`)
-    REFERENCES `school`.`students` (`student_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_enrollments_courses1`
-    FOREIGN KEY (`course_ID`)
-    REFERENCES `school`.`courses` (`course_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ALTER TABLE `school`.`instructors` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `school`.`courses` 
+ADD CONSTRAINT `fk_courses_instructors1`
+  FOREIGN KEY (`instructors_ID`)
+  REFERENCES `school`.`instructors` (`instructors_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE;
 
--- -----------------------------------------------------
--- Table `school`.`tags`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`tags` (
-  `tags_ID` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
-  PRIMARY KEY (`tags_ID`))
-ENGINE = InnoDB;
+ALTER TABLE `school`.`enrollments` 
+DROP FOREIGN KEY `fk_enrollments_students`;
 
+ALTER TABLE `school`.`enrollments` ADD CONSTRAINT `fk_enrollments_students`
+  FOREIGN KEY (`student_ID`)
+  REFERENCES `school`.`students` (`student_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_enrollments_courses1`
+  FOREIGN KEY (`course_ID`)
+  REFERENCES `school`.`courses` (`course_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE;
 
--- -----------------------------------------------------
--- Table `school`.`courses_tags`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `school`.`courses_tags` (
-  `course_ID` INT NOT NULL,
-  `tags_ID` INT NOT NULL,
-  INDEX `fk_courses_tags_courses1_idx` (`course_ID` ASC) VISIBLE,
-  INDEX `fk_courses_tags_tags1_idx` (`tags_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_courses_tags_courses1`
-    FOREIGN KEY (`course_ID`)
-    REFERENCES `school`.`courses` (`course_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_courses_tags_tags1`
-    FOREIGN KEY (`tags_ID`)
-    REFERENCES `school`.`tags` (`tags_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ALTER TABLE `school`.`courses_tags` 
+ADD CONSTRAINT `fk_courses_tags_courses1`
+  FOREIGN KEY (`course_ID`)
+  REFERENCES `school`.`courses` (`course_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+ADD CONSTRAINT `fk_courses_tags_tags1`
+  FOREIGN KEY (`tags_ID`)
+  REFERENCES `school`.`tags` (`tags_ID`)
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
